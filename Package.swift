0ab4661f25a889bc466f293a2ad996d16203fd1f
IgorKhomiak <ihorkhomyak@gmail.com>
2022-05-18 21:05:41 +0300

    temporary change branches

--- Package.swift
+++ Package.swift
@@ -15,11 +15,11 @@ let package = Package(
     
     dependencies: [
         // Dependencies declare other packages that this package depends on. feat/dccDateOfBirth
-        .package(name: "JsonLogic", url: "https://github.com/eu-digital-green-certificates/json-logic-swift.git", from: "1.2.2"),
+        .package(name: "JsonLogic", url: "https://github.com/eu-digital-green-certificates/json-logic-swift.git", .branch("feature/fix-date-formatter")), //from: "1.2.2"),
         .package(name: "SWCompression", url: "https://github.com/tsolomko/SWCompression.git", from: "4.7.0"),
         .package(name: "Alamofire", url: "https://github.com/Alamofire/Alamofire", from: "5.5.0"),
         .package(name: "JSONSchema", url: "https://github.com/eu-digital-green-certificates/JSONSchema.swift", .branch("master")),
-        .package(name: "CertLogic", url: "https://github.com/eu-digital-green-certificates/dgc-certlogic-ios",  from: "1.3.1"),
+        .package(name: "CertLogic", url: "https://github.com/eu-digital-green-certificates/dgc-certlogic-ios",  .branch("feature/fixed-date-formatter")), //from: "1.3.1"),
         .package(name: "DGCBloomFilter", url: "https://github.com/eu-digital-green-certificates/dgc-bloomfilter-ios.git", .branch("main")),
         .package(name: "DGCPartialVarHashFilter", url: "https://github.com/eu-digital-green-certificates/dgca-partialvarhashfilter-ios.git", .branch("main")),
         .package(name: "DGCCoreLibrary", url: "https://github.com/eu-digital-green-certificates/dgca-verification-core-library-ios.git", .branch("main")),
